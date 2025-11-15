7a4575dc55af11abd6b6ca3b8f9529b0638d2ffb
RiyadSheikh27 <135745417+RiyadSheikh27@users.noreply.github.com>
2025-11-15 17:51:47 +0600

    latest for chatting feature

--- api/urls.py
+++ api/urls.py
@@ -22,7 +22,7 @@ router.register(r'notifications', NotificationViewSet, basename='notification')
 
 urlpatterns = [
     path('start/', start_convo, name='start_convo'),
-    path('<int:convo_id>/', get_conversation, name='get_conversation'),
+    path('conversations/<int:convo_id>/', get_conversation, name='get_conversation'),
     path('conversations/', conversations, name='conversations'),
 
     path("", include(router.urls)),
